d8a7c36bf7d23f1242969532d93b19354c314aa9
Moisés Vieira <moisess.vieiraa.beltramee@gmail.com>
2022-09-09 09:37:52 -0300

    changing component styles

--- src/app/submit-intake/submit-intake.component.ts
+++ src/app/submit-intake/submit-intake.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-submit-intake',
-  template: '<button class="border border-white text-white rounded text-lg font-bold w-full mt-10 h-14 hover:bg-white hover:text-black transition-all duration-300 select-none" (click)="addIntakeNotifier.emit()">SUBMIT</button>',
+  template: '<button class="border border-white text-white rounded text-lg font-bold w-full mt-10 h-14 hover:border-blue-300 hover:shadow-sm shadow-blue-500/50 hover:text-blue-200 transition-all duration-300 select-none" (click)="addIntakeNotifier.emit()">SUBMIT</button>',
 })
 export class SubmitIntakeComponent implements OnInit {
 
