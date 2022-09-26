bf16e70de053f8226510300fc5c9254102cb1525
Moisés Vieira <moisess.vieiraa.beltramee@gmail.com>
2022-09-26 13:21:49 -0300

    refactoring submit intake component

--- src/app/submit-intake/submit-intake.component.ts
+++ src/app/submit-intake/submit-intake.component.ts
@@ -2,14 +2,17 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-submit-intake',
-  template: '<button class="border border-white text-white rounded text-lg font-bold w-full mt-10 h-14 hover:border-blue-300 hover:shadow-sm shadow-blue-500/50 hover:text-blue-200 transition-all duration-300 select-none" (click)="addIntakeNotifier.emit()">SUBMIT</button>',
+  templateUrl: './submit-intake.component.html',
 })
 export class SubmitIntakeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-  @Output() addIntakeNotifier = new EventEmitter();
+  @Output() add = new EventEmitter();
+
+  click() {
+    this.add.emit();
+  }
 }
