a1ec315f3efea7e226873f8451b8f795cc3cf2b5
Moisés Vieira <moisess.vieiraa.beltramee@gmail.com>
2022-12-09 19:55:53 -0300

    selecting cup when first added and scrolling div to show added cup

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CupIcon } from 'src/shared/models/cup-icon';
 import { Cup } from '../../shared/models/cup';
 
@@ -8,11 +8,17 @@ import { Cup } from '../../shared/models/cup';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
   readonly cupIcon = CupIcon;
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // if cup is selected when element is first loaded
+    // this means that if cup was added right now
+    if (this.cup.id === this.selectedCup) {
+      this.el.nativeElement.scrollIntoView();
+    }
+  }
 
   @Input() cup!: Cup;
   @Input() selectedCup?: string;
